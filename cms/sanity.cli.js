3062965822458cf624106dec1b8728a4515ee1fd
hoangviet1807 <hoangviet1807@gmail.com>
2026-07-20 23:46:41 +0700

    chore: update Sanity CLI configuration with new appId
    - Added appId to the deployment configuration in sanity.cli.js for improved project identification and management.
    - Ensured dataset configuration remains consistent with the production environment.

--- cms/sanity.cli.js
+++ cms/sanity.cli.js
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'sfqhf74q',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: "dh70il0i54nz3ft3liurkith"
   },
 })
